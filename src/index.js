90a12d1cc116ee1f1e29e58e9262c43a4f213d5e
Sagar Kumar <105171869+lionelraykai@users.noreply.github.com>
2026-03-31 19:00:43 +0530

    add env

--- src/index.js
+++ src/index.js
@@ -30,7 +30,7 @@ app.get('/', (req, res) => {
     res.send('Knotless API is running...');
 });
 
-const PORT = 5001;
+const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => {
     console.log(`Server running in ${process.env.NODE_ENV || 'development'} mode on port ${PORT}`);
